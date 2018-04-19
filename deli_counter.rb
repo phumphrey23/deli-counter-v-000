b196e7c525ec45e0afc317ef18811616f3fa37ce
Precious Humphrey <phumphrey23@gmail.com>
2018-04-19 01:51:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,11 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-
+  if katz_deli.length==0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "some stuff"
+  end
 end
 
 def line(katz_deli)
