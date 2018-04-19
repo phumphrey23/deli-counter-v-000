6a33551dbea363d4edc00186291981506b702179
Precious Humphrey <phumphrey23@gmail.com>
2018-04-19 01:30:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,15 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}.  You are number #{katz_deli.length} in line."
 end
 
+def now_serving(katz_deli)
+  
+end
+
 def line(katz_deli)
   if katz_deli.lenth==0
     puts "there is nobody in line"
   else
-    put "Hey"
+    katz_deli.each.with_index(1) do |name, index|
+      "The line is currently: #{index}. #{name}"
   end
 end
