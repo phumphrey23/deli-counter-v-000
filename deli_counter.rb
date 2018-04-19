4ebcbae5e344063f9b436670f2554887aa47542e
Precious Humphrey <phumphrey23@gmail.com>
2018-04-19 00:39:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,11 @@ katz_deli = []
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  "Welcome, #{name}.  You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}.  You are number #{katz_deli.length} in line."
+end
+
+def line(katz_deli)
+  if katz_deli.lenth==0
+    puts "there is nobody in line"
+  else
 end
