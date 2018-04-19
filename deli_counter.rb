07be4e936dc3956b89d095476ed7cc704b234d7d
Precious Humphrey <phumphrey23@gmail.com>
2018-04-19 02:26:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def line(katz_deli)
   if katz_deli.length==0
     puts "The line is currently empty."
   else
-    current_line.each.with_index(1) do |name, index|
+    katz_deli.each.with_index(1) do |name, index|
     current_line.push("#{index}. #{name}")
   end
     puts "The line is currently: #{current_line.join(" ")}"
